f57483e8bacd7183912f54f7122995e1d738b2ce
MadhanDevlpr <pmaadithya09@gmail.com>
2022-11-12 10:44:21 -0500

    update

---
+++ main/src/App.js
@@ -0,0 +1,16 @@
+import logo from './img/orb.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Saga Orb</h1>
+        <p>A visionary to reform the society in an absolute interactive way.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
