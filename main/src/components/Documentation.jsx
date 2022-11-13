3bc360d401e6696b15f76ed55ae211af06e42a71
MadhanDevlpr <pmaadithya09@gmail.com>
2022-11-13 01:16:01 -0500

    update

--- main/src/components/Documentation.jsx
+++ main/src/components/Documentation.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Documentation() {
   return (
     <div className="doc">
-        
+        <h1>Documentation</h1>
+        <p>Coming soon . . .</p>
+        <p>
+        <a href="/">Back to home</a>
+        </p>
     </div>
   )
 }
